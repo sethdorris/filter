8e9b935543cebdf7817ba3dbea84fb3a1c6c07c3
sethdorris <sethc.dorris@gmail.com>
2016-03-08 16:57:19 -0700

    "Updated the project with an HTTPRequest in order to populate DOM with some data to use the filter on, instead of hardcoding names"

--- filter.js
+++ filter.js
@@ -3,7 +3,25 @@ let header = document.getElementById("bindedheader");
 let input = document.getElementById("name");
 let nameheadings = document.getElementsByTagName("h4");
 let namediv = document.getElementById("names");
-		
+let hulkMovies;
+
+//Work to populate the DOM with at least Hulk Movies, and use the filter function on that data
+(function getHulkMovies() {
+		return new Promise( (resolve, reject) => {
+			let xhr = new XMLHttpRequest();
+			xhr.open("GET", "http://www.omdbapi.com/?s=hulk&r=json");
+			xhr.onload = resolve;
+			xhr.onerror = reject;
+			xhr.send();
+		}); 
+})().then( (data) => {
+	hulkMovies = data.target.response;
+	console.log("Hulks", hulkMovies)
+}).catch( (error) => {
+	console.log("error", error);
+});
+
+
 input.addEventListener("input", (e) => {
 	let searchby = input.value.toUpperCase();
 	let resultcount = 0;
@@ -25,6 +43,7 @@ input.addEventListener("input", (e) => {
 			nameheadings[i].style.display = 'block';
 		}
 	}
+	
 });
 
 
