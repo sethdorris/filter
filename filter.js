068ebe0da381a8085b11d64ec81444cefce7a0dc
Seth Dorris <sethc.dorris@gmail.com>
2016-03-09 15:51:47 +0000

    moved function declaration

--- filter.js
+++ filter.js
@@ -6,7 +6,7 @@ let namediv = document.getElementById("names");
 let hulkMovies;
 
 //Work to populate the DOM with at least Hulk Movies, and use the filter function on that data
-var getHulkMovies = function () {
+let getHulkMovies = function () {
 	return new Promise( (resolve, reject) => {
 		let xhr = new XMLHttpRequest();
 		xhr.open("GET", "https://www.omdbapi.com/?s=hulk");
@@ -15,6 +15,13 @@ var getHulkMovies = function () {
 		xhr.send();
 	}); 
 }
+
+let populateDomWithHulkMovies = function(movies) {
+	let moviesArray = movies.Search;
+	for (let i = 0; i < 10; i++) {
+		namediv.innerHTML += "<h4>" + moviesArray[i].Title + "</h4>";
+	}
+};
 
 getHulkMovies()
 .then( (data) => {
@@ -46,10 +53,3 @@ input.addEventListener("input", (e) => {
 		}
 	}
 });
-
-var populateDomWithHulkMovies = function(movies) {
-	let moviesArray = movies.Search;
-	for (let i = 0; i < 10; i++) {
-		namediv.innerHTML += "<h4>" + moviesArray[i].Title + "</h4>";
-	}
-};
